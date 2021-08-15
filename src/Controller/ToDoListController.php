36ad6be3d68171c5e7ac2ce8a495874f178ba0f1
Julien Paillassa <julien-paillassa_student2021@wilder.school>
2021-08-15 17:48:54 +0200

    Requis for test done

---
+++ src/Controller/ToDoListController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ToDoList;
+use App\Form\ToDoListType;
+use App\Repository\ToDoListRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ToDoListController extends AbstractController
+{
+    private $toDoListRepository;
+
+    public function __construct(ToDoListRepository $toDoListRepository)
+    {
+        $this->toDoListRepository = $toDoListRepository;
+    }
+
+    /**
+     * @Route("/", name="to_do_list_index")
+     */
+    public function index(): Response
+    {
+        $toDoList = $this->toDoListRepository->findAll();
+
+        return $this->render('to_do_list/index.html.twig', [
+            'website' => 'To Do List',
+            'to_do_list' => $toDoList,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="to_do_list_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $toDoList = new ToDoList();
+        $form = $this->createForm(ToDoListType::class, $toDoList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($toDoList);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('to_do_list_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('to_do_list/new.html.twig', [
+            'to_do_list' => $toDoList,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="to_do_list_show", methods={"GET"})
+     */
+    public function show(ToDoList $toDoList): Response
+    {
+        return $this->render('to_do_list/show.html.twig', [
+            'to_do_list' => $toDoList,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="to_do_list_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, ToDoList $toDoList): Response
+    {
+        $form = $this->createForm(ToDoListType::class, $toDoList);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('to_do_list_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('to_do_list/edit.html.twig', [
+            'to_do_list' => $toDoList,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="to_do_list_delete", methods={"POST"})
+     */
+    public function delete(Request $request, ToDoList $toDoList): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$toDoList->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($toDoList);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('to_do_list_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/ToDoList.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ToDoListRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ToDoListRepository::class)
+ */
+class ToDoList
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isDone;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getIsDone(): ?bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(bool $isDone): self
+    {
+        $this->isDone = $isDone;
+
+        return $this;
+    }
+}
